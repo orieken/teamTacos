bc356253f9698bf70874ecab4017550151302736
Jarod Adair <jarod.adair@manheim.com>
2015-08-07 22:54:23 -0500

    adding ng-repeat element

--- public/javascripts/team_taco/contact_cards.js
+++ public/javascripts/team_taco/contact_cards.js
@@ -1,8 +1,9 @@
 var app = angular.module('contactCards', []);
 app.controller('cardsCtrl', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-    $scope.fullName = function() {
-        return $scope.firstName + " " + $scope.lastName;
-    }
+    $scope.cards = [
+        {firstName:'Oscar', lastName:'Rieken'},
+        {firstName:'Umair', lastName:'Chagani'},
+        {firstName:'Wallace', lastName:'Harwood'}
+    ];
+
 });
