85e1ecd764742561032f35ec57d72e12e42197c8
Jarod Adair <jarod.adair@manheim.com>
2015-08-07 22:36:18 -0500

    add cards module

--- routes/cards.js
+++ routes/cards.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('contact cards');
+    res.render('cards', { title: 'Team Taco :: Contact Cards' });
 });
 
 module.exports = router;
