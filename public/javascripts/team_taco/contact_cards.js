c1a88ae66f4f54c4828ff3b3a0a7421132ddc756
Jarod Adair <adairjk@gmail.com>
2015-08-08 23:06:19 -0500

    picture pages .. picture pages

--- public/javascripts/team_taco/contact_cards.js
+++ public/javascripts/team_taco/contact_cards.js
@@ -1,5 +1,18 @@
 var app = angular.module('contactCards', []);
 app.controller('cardsCtrl', function($scope, $http) {
     $http.get("./allCards")
-        .success(function(response){$scope.cards = response['contacts'];});
+        .success(function(response){
+            for(contact in response['contacts']) {
+                var email = response['contacts'][contact]['email'];
+                if(email){
+                     var hash = md5(email);
+                    response['contacts'][contact]['image'] = "http://www.gravatar.com/avatar/" + hash;
+                } else {
+                    response['contacts'][contact]['image'] = "images/no-image.png";
+                }
+            }
+
+
+            $scope.cards = response['contacts'];
+        });
 });
